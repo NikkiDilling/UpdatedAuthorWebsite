8a8e321aada0a3fe9fc6f742144473b594f75f03
Nikki <ndh@peoplenet.dk>
2026-04-24 13:57:36 +0200

    add a working dictionary page (under development)

---
+++ app/Dictionary/page.tsx
@@ -0,0 +1,15 @@
+import dictionary from './dictionary.json';
+
+export default function DictionaryPage() {
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column", width: "90%", margin: "30px auto", justifyContent: "center", alignItems: "center", fontSize: "22px", padding: "0 20px" }}>
+
+            <h3>Demutriir Dictionary</h3>
+
+            <div>
+                
+            </div>
+        </div>
+    )
+}
