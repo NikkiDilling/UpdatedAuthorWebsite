b6a15895fcb7979ac4a0dba7f810e7751f493a2b
Nikki <ndh@peoplenet.dk>
2026-07-27 22:47:54 +0200

    little update

--- app/Books/prisoner-of-magnolia/page.tsx
+++ app/Books/prisoner-of-magnolia/page.tsx
@@ -1,5 +1,10 @@
 export default function PrisonerOfMagnoliaPage() {
     return (
-        <div>This page is under construction</div>
+        <div style={{ textAlign: "center" }}>
+
+            <h2>This page is under construction</h2>
+
+            {/* this page has neat buttons that show playlist, dictionary etc. */}
+        </div>
     )
 }
