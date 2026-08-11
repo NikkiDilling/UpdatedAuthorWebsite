bae38017e904e5d6fdc3a95d309c204ff198c5e9
Nikki <ndh@peoplenet.dk>
2026-08-11 17:09:09 +0200

    book page update

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -16,33 +16,40 @@ export default function ProjectSunsetPage() {
 
           <div className={classes.textContainer}>
 
-            <h2 className={classes.intro}>
-              <div>Enter a world of elves, elemental spirits and delicious villains!</div>
-              <br />
-              <div>Start the journey with book 1: <span className={classes.bookTitle}>Prisoner of Magnolia</span> .</div>
+            <div className={classes.intro}>
 
-            </h2>
+              <Image
+                className={classes.bookcover}
+                src={bookcover}
+                alt="book cover"
+              />
 
-            <Image
-              src={bookcover}
-              alt="book cover"
-            />
 
-            <h3 style={{ margin: "20px auto" }}>Synopsis</h3>
-            <div className={classes.text}>
-              The elven realm of Gaudelir hasn’t known war for a century since a fire spirit ripped the continent in two. But the balance of power changes the day of Princess Elayah’s elemental ceremony.
+              <div>
+                <div className={classes.title}>Prisoner Of Magnolia Series</div>
+                <div className={classes.subtitle}>Enter a world of elves, elemental spirits and delicious villains!</div>
+
+                <div>
+                  <div className={classes.text}>
+                    The elven realm of Gaudelir hasn’t known war for a century since a fire spirit ripped the continent in two. But the balance of power changes the day of Princess Elayah’s elemental ceremony.
+                  </div>
+                  <div className={classes.text}>
+                    Elayah Lorethain has never had any aptitude for magic. When she is expected to venture into her subconscious to retrieve her elemental power, she is convinced she will fail. But when she returns, her power isn’t the only thing she brings back.
+                  </div>
+
+                  <div className={classes.text}>
+                    A darkness follows her, filling her mind with vicious whispers and trying to claw its way out. Elayah is weak. Naive. She isn't ready to rule and that's why her parents are still hiding things from her in the archive beneath the palace. And with tensions brewing between her and her childhood best friend and guard—Tayrem—she’s torn between duty and love.
+                  </div>
+
+                  <div className={classes.text}>
+                    When the enemy kingdom invades, and Tayrem’s life is on the line, there is only one way she can save him. Forced to marry the enemy king, Elayah has to face the darkness within herself, the secrets of her kingdom, and the desires of her heart that she's been pushing down for decades.
+                  </div>
+                </div>
+              </div>
+
+
             </div>
-            <div className={classes.text}>
-              Elayah Lorethain has never had any aptitude for magic. When she is expected to venture into her subconscious to retrieve her elemental power, she is convinced she will fail. But when she returns, her power isn’t the only thing she brings back.
-            </div>
 
-            <div className={classes.text}>
-              A darkness follows her, filling her mind with vicious whispers and trying to claw its way out. Elayah is weak. Naive. She isn't ready to rule and that's why her parents are still hiding things from her in the archive beneath the palace. And with tensions brewing between her and her childhood best friend and guard—Tayrem—she’s torn between duty and love.
-            </div>
-
-            <div className={classes.text}>
-              When the enemy kingdom invades, and Tayrem’s life is on the line, there is only one way she can save him. Forced to marry the enemy king, Elayah has to face the darkness within herself, the secrets of her kingdom, and the desires of her heart that she's been pushing down for decades.
-            </div>
           </div>
 
           <div className={classes.quotes}>
@@ -72,12 +79,12 @@ export default function ProjectSunsetPage() {
               <CircularProgress
                 enableTrackSlot
                 variant="determinate"
-                value={98}
+                value={100}
                 aria-label="Export data"
               />
               <div>
                 <div>Book 1 status:</div>
-                <div>Formatting</div>
+                <div>Done!</div>
               </div>
 
             </div>
